f48d23c69ea99ddf4079ea79290e8b6ec64ecb26
lucas z <lucasfelipezimmermmann@gmail.com>
2022-07-08 11:42:30 +0200

    Changing script and improving communication

--- installation_script_laravel.sh
+++ installation_script_laravel.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-echo "Starting..."
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+reset=`tput sgr0`
 
-echo "[1/8] Installing APACHE"
+echo "${yellow}Starting...${reset}"
+echo ""
+
+echo "${green}[1/8] Installing APACHE${reset}"
+echo ""
+sudo apt-get update
 sudo apt-get install apache2 -y 
 
-echo "[2/8] Instaling PHP"
-echo "[QUESTION] Type the version of PHP that you want to install (ex: 7.2):"
+echo "${green}[2/8] Instaling PHP${reset}"
+echo ""
+echo "${yellow}[QUESTION] Type the version of PHP that you want to install:${reset}"
+echo ""
 read phpVersion
-
+echo ""
 sudo add-apt-repository ppa:ondrej/php  -y 
 sudo apt-get install php$phpVersion -y 
 sudo apt-get update 
@@ -27,53 +38,60 @@ sudo apt-get install curl -y
 sudo apt-get install openssl -y
 sudo a2enmod rewrite
 
-
-echo "[3/8] Instaling Componser"
+echo ""
+echo "${green}[3/8] Instaling Componser${reset}"
+echo ""
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 sudo service apache2 restart
-echo "[4/8] Generating SSH Key"
+
 echo ""
-echo ""
+echo "${green}[4/8] Generating SSH Key${reset}"
 ssh-keygen
+echo ""
 cat ~/.ssh/id_rsa.pub
-echo "[ACTION] Copy the Key and Add in project at Github"
-echo "[QUESTION] Type 'YES' if you did that:"
+echo ""
+echo "${blue}[ACTION] Copy the Key and Add in project at Github${reset}"
+echo "${yellow}[QUESTION] Type 'YES' if you did that:${reset}"
 read answer
 rm -rf /var/www/*
-echo "[5/8] Cloning the repository"
-echo "[QUESTION] Type the repository (ex: git@github.com:project/site.git):"
+
+echo ""
+echo "${green}[5/8] Cloning the repository${reset}"
+echo ""
+echo "${yellow}[QUESTION] Type the repository:${reset}"
 read gitRepository
 cd /var/www
 git init  
 git remote add origin $gitRepository
 git pull origin master 
-echo "[6/8] Project Composer install"
 
+echo ""
+echo "${green}[6/8] Project Composer install${reset}"
+echo ""
 sudo composer install --ignore-platform-reqs
 sudo chmod -R 777 bootstrap/cache
 sudo chmod -R 777  /var/www/storage/logs
 sudo chmod -R 777 storage
 
-echo "[7/8] Updating the files of Apache"
-curl -O https://raw.githubusercontent.com/luucasfzs/ubunto-20-laravel/master/installation_script_laravel.sh && sh installation_script_laravel.sh
+echo ""
+echo "${green}[7/8] Update the files of Apache${reset}"
+echo ""
+curl -O https://raw.githubusercontent.com/luucasfzs/ubunto-20-laravel/master/files/virtualhost.txt
+sudo  mv virtualhost.txt /etc/apache2/sites-available/000-default.conf
 curl -O https://raw.githubusercontent.com/luucasfzs/ubunto-20-laravel/master/files/apache2.txt
 sudo  mv apache2.txt /etc/apache2/apache2.conf
-curl -O https://raw.githubusercontent.com/luucasfzs/ubunto-20-laravel/master/logs/env_example.txt
-sudo  mv env_example.txt .env
+sudo service apache2 restart
 
-
-echo "[8/8] Instaling NODE"
-echo "[QUESTION] Type the version of NODE that you want to install (ex: 14):"
+echo ""
+echo "${green}[8/8] Instaling NODE${reset}"
+echo "${yellow}[QUESTION] Type the version of NODE that you want to install:${reset}"
+echo ""
 read nodeVersion
 curl -sL https://deb.nodesource.com/setup_$nodeVersion.x | sudo bash -
 sudo apt -y install nodejs
 sudo npm install
 sudo npm run prod
 
-sudo service apache2 restart
-php artisan key:generate
-echo "Done"
-
-
-
+echo ""
+echo "${green}Finish${reset}"
